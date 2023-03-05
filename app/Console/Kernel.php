063d5bc0d63b8f8954024302ad6df3224b5b9127
Md. Shahnawaz Ahmed <shahnawazdaanish@gmail.com>
2023-03-06 02:25:23 +0600

    Added Log in Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        Log::info('Scheduler Works!');
         // $schedule->command('inspire')->hourly();
         $schedule->command('queue:work --stop-when-empty')
             ->everyMinute()
